195978dfb0ae7bae124689cacfbf054fa59b1272
lamyinia <142591822+lamyinia@users.noreply.github.com>
2025-07-14 01:07:03 +0800

    前端搭建

---
+++ TellYou/backend/pojo/src/main/java/org/com/pojo/PojoApplication.java
@@ -0,0 +1,14 @@
+package org.com.pojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PojoApplication.class, args);
+    }
+
+}
